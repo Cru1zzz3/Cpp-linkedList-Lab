198461ee6880217a4bff612f85c8fd3b93dbdcc7
Cru1zzz3 <udartzev_sv@mail.ru>
2018-03-20 03:42:18 +0300

    Added header and .cpp file with realization of linkedListClass

---
+++ linkedListClass.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include "linkedListClass.h"
+
+using namespace std;
+
+linkedListClass* linkedListClass::inputElement(linkedListClass *currentElement){
+		int x;
+		cout << "Input new element: ";
+		cin >> x;
+		cout << endl;
+
+		linkedListClass *newElement = new linkedListClass();
+
+		if (currentElement->next != nullptr){
+			newElement->data = x;
+			newElement->next = currentElement->next;
+			currentElement->next = newElement;
+			newElement->previous = currentElement;
+			currentElement= newElement->next;
+			currentElement->previous = newElement->previous;
+			currentElement = newElement;
+		}
+	
+		else {
+			newElement->data = x;
+			newElement->previous = currentElement;
+
+			currentElement->next = newElement;
+			currentElement = newElement;		
+		}
+	    
+		return (currentElement);
+	};
+
+linkedListClass* linkedListClass::findElement(linkedListClass *rootElement){
+		int x;
+		cout << "Find element with data: ";
+		cin >> x;
+		cout << endl;
+
+		linkedListClass *findPointer = rootElement;
+
+		while (findPointer != nullptr){
+			if(findPointer->data != x){
+				findPointer = findPointer->next;
+			}
+			else {
+				cout << "Element with data "<< findPointer->data << " was found!" << endl;
+				cout << "Previous element adress: " << findPointer->previous << "\t Data: " << findPointer->data << " / Data adress: "<< findPointer << "\t Next element adress:\t " << findPointer->next<< endl << endl;
+				return (findPointer);
+			 
+			} 
+		}
+		cout << " No element with data "<< findPointer->data << endl;
+		return(rootElement);
+	};
+
+linkedListClass* linkedListClass::deletingElement(linkedListClass *currentElement){
+	
+		linkedListClass *deleteElement = currentElement;
+		int tempData = deleteElement->data;
+		 
+		 if (deleteElement->next == nullptr){
+			currentElement = deleteElement->previous;
+			currentElement->next = nullptr;
+		
+		}
+		 else {
+			currentElement = currentElement->previous;
+			currentElement ->next = deleteElement->next;
+			currentElement = currentElement->next;
+			currentElement ->previous = deleteElement->previous;
+		}
+
+		delete(deleteElement);
+		cout << "Element with data "<< tempData << " sucssessfully deleted!" << endl;
+		return (currentElement);
+
+    
+	}
+
+linkedListClass* linkedListClass::deletingRootElement(linkedListClass *rootElement){
+
+		linkedListClass *deleteElement = rootElement;
+		if (deleteElement->next == nullptr){
+		cout << "Can't delete last element of list!!!"<< endl;
+		return (rootElement);
+		}
+
+		linkedListClass *currentElement = deleteElement->next;
+
+		int tempData = deleteElement->data;
+		currentElement->previous = nullptr;
+
+		delete(deleteElement);
+		cout << "Root element with data "<< tempData << " sucssessfully deleted!" << endl;
+		return (currentElement);
+	
+	};
+
+void  linkedListClass::showList(linkedListClass *rootElement){
+
+		if (rootElement != nullptr){
+			linkedListClass *showPointer = rootElement;
+			do {
+				cout << "Previous element adress: " << showPointer->previous << "\t Data: " << showPointer->data << " / Data adress: "<< showPointer << "\t Next element adress:\t " << showPointer->next<< endl;
+				showPointer = showPointer->next;
+			} while (showPointer != nullptr);
+			cout << "\n" << endl;
+		} else {
+			cout << "Linked list is empty! Input root element!" << endl;
+		}
+	}
+
--- linkedListClass.h
+++ linkedListClass.h
@@ -2,13 +2,22 @@
 #define LINKEDLISTCLASS_H
 
 
-
-
 class linkedListClass{
+	
+	private:
+		
+	protected:
 	public:
 		linkedListClass *previous;
 		int data;
 		linkedListClass *next;
-	};
+
+		linkedListClass*  inputElement(linkedListClass *currentElement);
+
+		linkedListClass* findElement(linkedListClass *rootElement);
+		linkedListClass* deletingElement(linkedListClass *currentElement);
+		linkedListClass* deletingRootElement(linkedListClass *rootElement);
+		void showList(linkedListClass *rootElement);
+};
 
 #endif LINKEDLISTCLASS_H
